be467d24478ed85fbe159a524b5c78db19584e3f
pronix <proenix575@gmail.com>
2020-03-24 19:20:08 +0300

    change router-link hover style

--- src/components/menu.js
+++ src/components/menu.js
@@ -6,7 +6,7 @@ const menu = new View(
       <div style="margin: 0; padding: 0; font-weight: bold; font-size: 25px; width: 100%">Todo<span style="color: var(--main-block-background)">List</span></div>
       <div class="routerLink flex routerLink-todos"><span>Todos</span><i class="fa fa-sticky-note-o"></i></div>
       <div class="routerLink flex routerLink-settings"><span>Settings</span><i class="fa fa-gear"></i></div>
-      <div class="routerLink flex routerLink-dashboard"><span>Dashboard</span><i class="fa fa-gear"></i></div>
+      <div class="routerLink flex routerLink-dashboard"><span>Dashboard</span><i class="fa fa-area-chart"></i></div>
     </div>
   `,
 
